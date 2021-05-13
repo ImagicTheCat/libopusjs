8d8ad807cece9132b8b985d6a021b7055e12cb8c
Chris Bainbridge <chris.bainbridge@speech-graphics.com>
2021-05-13 16:12:23 +0100

    Fix compile with opus-1.3.1 and emscripten-2.0.17
    - Use latest stable upstream opus-1.3.1.
    
    - Disable default build options not supported by emscripten
      (--disable-stack-protector --disable-hardening).
    
    - Fix runtime error `_free is not a function`.
      Emscripten no longer exports _free by default, so explicitly export it.

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 # usage: ./build.sh [flags]
 # asm.js release flags: -O2 --memory-init-file 0
 # wasm release flags: -O2 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1
-em++ api.cpp -Iopus/include -Lopus/.libs -lopus --pre-js preapi.js --post-js api.js $@ -o libopus.js
+em++ api.cpp -Iopus/include -Lopus/.libs -lopus --pre-js preapi.js \
+  --post-js api.js -s EXPORTED_FUNCTIONS='["_free"]' $@ -o libopus.js
